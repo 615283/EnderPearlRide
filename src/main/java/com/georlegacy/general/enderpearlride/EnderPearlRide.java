cd608f5eb3c0b705b9a916ee567d802e7890486e
James <615283@users.noreply.github.com>
2018-03-28 17:23:23 +0100

    Added the plugin :accept:

--- src/main/java/com/georlegacy/general/enderpearlride/EnderPearlRide.java
+++ src/main/java/com/georlegacy/general/enderpearlride/EnderPearlRide.java
@@ -1,9 +1,12 @@
 package com.georlegacy.general.enderpearlride;
 
+import com.georlegacy.general.enderpearlride.events.EnderThrowEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EnderPearlRide extends JavaPlugin {
 
-
-
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new EnderThrowEvent(), this);
+    }
 }
---
+++ src/main/java/com/georlegacy/general/enderpearlride/events/EnderThrowEvent.java
@@ -0,0 +1,23 @@
+package com.georlegacy.general.enderpearlride.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+
+public class EnderThrowEvent implements Listener {
+
+    @EventHandler
+    public void onThrow(ProjectileLaunchEvent e) {
+        Bukkit.broadcastMessage("in");
+        if (e.getEntityType().equals(EntityType.ENDER_PEARL)) {
+            if (!(e.getEntity().getShooter() instanceof Player)) return;
+            if (((Player) e.getEntity().getShooter()).hasPermission("enderpearlride.ride")) {
+                e.getEntity().addPassenger((Player) e.getEntity().getShooter());
+            }
+        }
+    }
+
+}
